db1cad040a44c655be7b31a270a8f6b3c39c41d5
Luis Ayala <saltwaterlou@yahoo.com>
2022-12-06 18:45:02 -0600

    Added js scripting

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var computerChoice = Math.floor(Math.random() *3);
 // Displays what the computer chose
 window.alert("The computer chose  "   + choices[computerChoice]);
 // Tells the user if they won or lost 
+// Ask if they want to play again, confirm
 if (computerChoice === [0,2]) {
     window.prompt("You have lost")
     }
