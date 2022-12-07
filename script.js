63fc3f6be55bf1c52e4df6a968202c2a91278bb9
Luis Ayala <saltwaterlou@yahoo.com>
2022-12-06 18:42:34 -0600

    Added windows.alert

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // Ask the user to choose R, P, or S
-var input = window.prompt("R, P, or S)
+var input = window.prompt("R, P, or S")
 console.log(input);
 // Computer chooses R, P, or S
 var choices = ["Rock", "Paper", "Scissors"];
 var computerChoice = Math.floor(Math.random() *3);
 // Displays what the computer chose
-window.alert("The computer chose" + choices[computerChoice]);
+window.alert("The computer chose  "   + choices[computerChoice]);
 // Tells the user if they won or lost 
-
+if (computerChoice === [0,2]) {
+    window.prompt("You have lost")
+    }
+else {
+    windows.prompt("You're the winner")
+}
                           
